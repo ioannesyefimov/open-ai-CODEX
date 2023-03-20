1525de1b1ba36fef9ae7d487ae423b0d474318bc
Ioannes <ivan.efimov2281@gmail.com>
2023-03-20 20:54:04 +0300

    deleted html plugin

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,10 @@
 import dynamicImport from 'vite-plugin-dynamic-import'
 
 
-const htmlImport = {
-    name: "htmlImport",
-    /**
-     * Checks to ensure that a html file is being imported.
-     * If it is then it alters the code being passed as being a string being exported by default.
-     * @param {string} code The file as a string.
-     * @param {string} id The absolute path. 
-     * @returns {{code: string}}
-     */
-    transform(code, id) {
-      if (/^.*\.html$/g.test(id)) {
-        code = `export default \`${code}\``
-      }
-      return { code }
-    }
-  }
+
   
   export default {
-    plugins: [ htmlImport, dynamicImport()
+    plugins: [  dynamicImport()
         ]
    
   }
